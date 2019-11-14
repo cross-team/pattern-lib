77f37075045d1f75374ba850344925a5167b164a
Marcello Paiva <marcello.paiva@gmail.com>
2019-11-14 16:19:09 -0500

    added github issues as datasource

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,35 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-source-github',
+      options: {
+        headers: {
+          Authorization: `Bearer f2422ee381499ffed215fa941eb50d7250f12867`, // https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/
+        },
+        queries: [
+          `{
+            repository(owner: "cross-team", name: "pattern-lib") {
+              issues(last: 20, states: OPEN) {
+                edges {
+                  node {
+                    id
+                    author {
+                      avatarUrl
+                      login
+                      url
+                    }
+                    bodyHTML
+                    title
+                    url
+                  }
+                }
+              }
+            }
+          }`,
+        ],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
