d70576d962dc006c2b8780ab9d06e9cdcea2d4be
Marcello Paiva <marcello.paiva@gmail.com>
2019-12-05 15:19:46 -0500

    built banner for pattern page

--- src/templates/pattern.js
+++ src/templates/pattern.js
@@ -1,20 +1,45 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../components/layout'
-import { makeStyles, Paper, Typography } from '@material-ui/core'
+import { makeStyles, Paper, Typography, Chip } from '@material-ui/core'
+import { ClientError } from 'graphql-request'
 
 const useStyles = makeStyles(theme => ({
   banner: {
     width: '100%',
-    height: '200px',
+    minHeight: '200px',
     display: 'flex',
-    alignItems: 'center',
+    justifyContent: 'space-between',
     backgroundColor: '#333333',
-    borderTop: 'solid 1px #ff8001',
+    color: '#fff',
   },
   title: {
-    color: '#fff',
     margin: theme.spacing(2),
+  },
+  titleContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  classifiers: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'flex-end',
+    marginRight: theme.spacing(1),
+  },
+  chipContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  rightMargin: {
+    marginRight: theme.spacing(1),
+  },
+  categoryContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 }))
 
@@ -25,7 +50,26 @@ export default ({data}) => {
   return (
     <Layout >
       <Paper className={classes.banner}>
-        <Typography variant='h2' className={classes.title}>{ frontmatter.title ? frontmatter.title : '' }</Typography>
+        <div className={classes.titleContainer}>
+          <Typography variant='h2' className={classes.title}>{ frontmatter.title ? frontmatter.title : '' }</Typography>
+          <Typography className={classes.title}>{ frontmatter.caption ? frontmatter.caption : '' }</Typography>
+        </div>
+        <div className={classes.classifiers}>
+          <div className={classes.categoryContainer}>
+            <Typography variant='h6' className={classes.rightMargin}>Category:</Typography>
+            <Typography variant='h6' className={classes.rightMargin}>{ frontmatter.category ? frontmatter.category : '' }</Typography>
+          </div>
+          <div className={classes.categoryContainer}>
+            <Typography className={classes.rightMargin}>Subcategory:</Typography>
+            <Typography className={classes.rightMargin}>{ frontmatter.subcategory ? frontmatter.subcategory : '' }</Typography>
+          </div>
+          <div className={classes.chipContainer}>
+            <Typography className={classes.rightMargin}>Tags:</Typography>
+            { frontmatter.tags.map((tag) => 
+              <Chip label={tag} color="primary" className={classes.rightMargin} />
+            ) }
+          </div>
+        </div>
       </Paper>
     </Layout>
   )
@@ -37,6 +81,10 @@ query($path: String!) {
       html
       frontmatter {
         title
+        caption
+        category
+        subcategory
+        tags
       }
       fields {
         slug
