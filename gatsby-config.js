5cbb96f36e72a1cdc0d7456ec1c85d250d262447
Sebastian <sebastian@softdev.team>
2019-12-09 14:36:12 -0500

    trying with options

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,7 +67,10 @@ module.exports = {
         },
       },
     },
-    `gatsby-plugin-material-ui`,
+    {
+      resolve: 'gatsby-plugin-material-ui',
+      options: { stylesProvider: { injectFirst: true, }, }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
