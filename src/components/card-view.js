374496d77c7384be24238a3a2f41d6bf159e4229
Marcello Paiva <marcello.paiva@gmail.com>
2019-12-16 15:17:16 -0500

    fixed imports

--- src/components/card-view.js
+++ src/components/card-view.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import { Chip, Divider } from '@material-ui/core';
+import {
+  makeStyles,
+  Card,
+  CardHeader,
+  CardContent,
+  CardMedia,
+  IconButton,
+  Typography,
+  Chip,
+  Divider
+} from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
   card: {
